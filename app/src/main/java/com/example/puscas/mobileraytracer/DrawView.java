47f6ae21864f21c93975e2a2312497a90e79506c
PTPuscas <ni_net@hotmail.com>
2016-11-11 02:15:10 +0000

    Added a message handler to the MainActivity thread.

--- app/src/main/java/com/example/puscas/mobileraytracer/DrawView.java
+++ app/src/main/java/com/example/puscas/mobileraytracer/DrawView.java
@@ -2,9 +2,10 @@ package com.example.puscas.mobileraytracer;
 
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.graphics.Bitmap.Config;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.os.Handler;
+import android.os.Message;
 import android.os.SystemClock;
 import android.util.AttributeSet;
 import android.view.View;
@@ -17,9 +18,9 @@ public class DrawView extends View
     private long start_;
     private long renderTime_;
     private TextView textView_;
-
     private Bitmap bitmapW_;
     private Bitmap bitmapR_;
+    private Handler handler_;
 
     public DrawView(Context context, AttributeSet attrs)
     {
@@ -27,47 +28,52 @@ public class DrawView extends View
     }
 
     private native void initialize(int scene, int shader, int width, int height);
-
     private native void drawIntoBitmap(Bitmap image);
-
     private native int isWorking();
-
     private native void finished();
+
+    public void setHandler(Handler pHandle) {
+        handler_ = pHandle;
+    }
 
     public void createScene(int scene, int shader, TextView textView)
     {
         textView_ = textView;
-        this.bitmapW_ = createBitmap(this.getWidth(), this.getHeight(), Config.ARGB_8888);
-        this.initialize(scene, shader, this.getWidth(), this.getHeight());
+        bitmapW_ = createBitmap(getWidth(), getHeight(), Bitmap.Config.ARGB_8888);
+        initialize(scene, shader, getWidth(), getHeight());
         bitmapW_.eraseColor(Color.BLUE);
     }
 
     @Override
     public void onDraw(Canvas canvas)
     {
-        if (this.isInEditMode() == false)
+        if (isInEditMode() == false)
         {
-            switch (this.isWorking()) {
+            switch (isWorking()) {
                 case 0://Iniciar renderizaçao
                     start_ = SystemClock.elapsedRealtime();
-                    this.drawIntoBitmap(bitmapW_);//correr ray tracer no bitmapW
-                    this.invalidate();
+                    drawIntoBitmap(bitmapW_);//correr ray tracer no bitmapW
+                    invalidate();
                     break;
 
                 case 1://Enquanto ray-tracer ainda está a funcionar
                     bitmapR_ = createBitmap(bitmapW_);//copiar bitmap
-                    canvas.drawBitmap(bitmapR_, 0.0f, 0.0f, null);//desenhar bitmapR
-                    renderTime_ = SystemClock.elapsedRealtime() - start_;
-                    textView_.setText("w:" + this.getWidth() + ", h:" + this.getHeight() + ", t:" + renderTime_ + " ms");
-                    this.invalidate();
+                    canvas.drawBitmap(this.bitmapR_, 0.0f, 0.0f, null);//desenhar bitmapR
+                    renderTime_ = SystemClock.elapsedRealtime() - this.start_;
+                    textView_.setText("Rendering -> w:" + getWidth() + ", h:" + getHeight() + ", t:" + renderTime_ + " ms");
+                    invalidate();
                     break;
 
                 case 2://Quando ray-tracer acabar
-                    renderTime_ = SystemClock.elapsedRealtime() - start_;
-                    this.finished();
-                    canvas.drawBitmap(bitmapW_, 0.0f, 0.0f, null);//desenhar bitmapW
-                    textView_.setText("w:" + this.getWidth() + ", h:" + this.getHeight() + ", t:" + renderTime_ + " ms");
+                    renderTime_ = SystemClock.elapsedRealtime() - this.start_;
+                    finished();
+                    canvas.drawBitmap(this.bitmapW_, 0.0f, 0.0f, null);//desenhar bitmapW
+                    textView_.setText("Rendered -> w:" + getWidth() + ", h:" + getHeight() + ", t:" + renderTime_ + " ms");
                     renderTime_ = 0;
+                    bitmapW_.recycle();
+                    bitmapR_.recycle();
+                    Message completeMessage = this.handler_.obtainMessage(1);
+                    completeMessage.sendToTarget();
                     break;
 
                 default:
--- app/src/main/java/com/example/puscas/mobileraytracer/MainActivity.java
+++ app/src/main/java/com/example/puscas/mobileraytracer/MainActivity.java
@@ -2,13 +2,12 @@ package com.example.puscas.mobileraytracer;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.TextView;
-
-import com.example.puscas.mobileraytracer.R.id;
-import com.example.puscas.mobileraytracer.R.layout;
 
 public class MainActivity extends Activity
 {
@@ -21,49 +20,44 @@ public class MainActivity extends Activity
     private DrawView drawView_;
     private int scene_;
     private int shader_;
+    private MainActivity.MessageHandler handler_;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        this.setContentView(layout.activity_main);
+        setContentView(R.layout.activity_main);
 
-        textView_ = (TextView) this.findViewById(id.timeText);
-        drawView_ = (DrawView) this.findViewById(id.viewDraw);
-        mRenderButton_ = (Button) this.findViewById(id.renderButton);
-
+        textView_ = (TextView) findViewById(R.id.timeText);
+        drawView_ = (DrawView) findViewById(R.id.viewDraw);
+        handler_ = new MainActivity.MessageHandler();
+        drawView_.setHandler(handler_);
+        mRenderButton_ = (Button) findViewById(R.id.renderButton);
         drawView_.setVisibility(View.INVISIBLE);
     }
 
     public void startRender(View view)
     {
         mRenderButton_.setEnabled(false);
-        drawView_.invalidate();
         drawView_.createScene(scene_, shader_, textView_);
+        drawView_.invalidate();
         drawView_.setVisibility(View.VISIBLE);
-        mRenderButton_.setEnabled(true);
     }
 
     public void onSceneRadioButtonClicked(View view)
     {
         // Is the button now checked?
-        boolean checked = ((RadioButton) view).isChecked();
+        if (((RadioButton) view).isChecked() == false) return;
 
         // Check which radio button was clicked
         switch (view.getId())
         {
-            case id.radioCornell:
-                if (checked)
-                {
-                    scene_ = 0; // cornell
-                }
+            case R.id.radioCornell:
+                this.scene_ = 0; // cornell
                 break;
 
-            case id.radioSpheres:
-                if (checked)
-                {
-                    scene_ = 1; // spheres
-                }
+            case R.id.radioSpheres:
+                this.scene_ = 1; // spheres
                 break;
 
             default:
@@ -74,27 +68,36 @@ public class MainActivity extends Activity
     public void onShaderRadioButtonClicked(View view)
     {
         // Is the button now checked?
-        boolean checked = ((RadioButton) view).isChecked();
+        if (((RadioButton) view).isChecked() == false) return;
 
         // Check which radio button was clicked
         switch (view.getId())
         {
-            case id.radioNoShadows:
-                if (checked)
-                {
-                    shader_ = 0; // No Shadows
-                }
+            case R.id.radioNoShadows:
+                this.shader_ = 0; // No Shadows
                 break;
 
-            case id.radioWhitted:
-                if (checked)
-                {
-                    shader_ = 1; // Whitted
-                }
+            case R.id.radioWhitted:
+                this.shader_ = 1; // Whitted
                 break;
 
             default:
                 break;
+        }
+    }
+
+    private class MessageHandler extends Handler {
+        @Override
+        public void handleMessage(Message inputMessage) {
+            switch (inputMessage.what) {
+                case 1:        // Render finished
+                    MainActivity.this.mRenderButton_.setEnabled(true);
+                    break;
+
+                default:
+                    super.handleMessage(inputMessage);
+                    break;
+            }
         }
     }
 }
