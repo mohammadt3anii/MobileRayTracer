10dfefa81102df3e353ff15224c3cb575460d3d3
PTPuscas <ni_net@hotmail.com>
2018-02-13 18:42:10 +0000

    Changed jniLibs folder name to Scripts.

--- Scripts/profile.sh
+++ Scripts/profile.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-PLOT_SCRIPTS="Plot_Scripts"
+MOBILERT_DIR="/mnt/D/Android_Studio_Projects/MobileRayTracer"
+BIN_DIR="${MOBILERT_DIR}/build_native/bin"
+SCRIPTS_DIR="${MOBILERT_DIR}/Scripts"
+PLOT_SCRIPTS_DIR="${SCRIPTS_DIR}/Plot_Scripts"
+OBJS_DIR="${MOBILERT_DIR}/app/src/main/assets/WavefrontOBJs"
+
+MOBILERT_SRCS="${MOBILERT_DIR}/app/Components/MobileRT/src"
+COMPONENTS_SRCS="${MOBILERT_DIR}/app/Components/src"
+GTK_INTERFACE_SRCS="${MOBILERT_DIR}/app/GTK_Interface"
+SCENES_SRCS="${MOBILERT_DIR}/app/Scenes"
+COMPONENTS_THIRDPARTY="${MOBILERT_DIR}/app/Components/third_party"
+
+GTK_HEADERS=$(pkg-config --cflags gtk+-2.0)
+GTK_HEADERS=${GTK_HEADERS//-I/-isystem}
 
 if [ -z "${PLOT_GRAPHS}" ]; then
   PLOT_GRAPHS="Plot_Graphs"
@@ -12,12 +25,12 @@ do
   FILES+=($(find ${FOLDER} -type f))
 done
 
-#OBJ="/mnt/D/Android_Studio_Projects/MobileRayTracer/app/src/main/assets/WavefrontOBJs/CornellBox/CornellBox-Sphere.obj"
-#MTL="/mnt/D/Android_Studio_Projects/MobileRayTracer/app/src/main/assets/WavefrontOBJs/CornellBox/CornellBox-Sphere.mtl"
-OBJ="/mnt/D/Android_Studio_Projects/MobileRayTracer/app/src/main/assets/WavefrontOBJs/conference/conference.obj"
-MTL="/mnt/D/Android_Studio_Projects/MobileRayTracer/app/src/main/assets/WavefrontOBJs/conference/conference.mtl"
-#OBJ="/mnt/D/Android_Studio_Projects/MobileRayTracer/app/src/main/assets/WavefrontOBJs/CornellBox/CornellBox-Empty-CO2.obj"
-#MTL="/mnt/D/Android_Studio_Projects/MobileRayTracer/app/src/main/assets/WavefrontOBJs/CornellBox/CornellBox-Empty-CO.mtl"
+#OBJ="${OBJS_DIR}/CornellBox/CornellBox-Sphere.obj"
+#MTL="${OBJS_DIR}/CornellBox/CornellBox-Sphere.mtl"
+OBJ="${OBJS_DIR}/conference/conference.obj"
+MTL="${OBJS_DIR}/conference/conference.mtl"
+#OBJ="${OBJS_DIR}/CornellBox/CornellBox-Empty-CO2.obj"
+#MTL="${OBJS_DIR}/CornellBox/CornellBox-Empty-CO.mtl"
 
 export ASAN_OPTIONS="suppressions=sanitizer_ignore.suppr:verbosity=1:strict_string_checks=1:detect_stack_use_after_return=1:check_initialization_order=1:strict_init_order=1:halt_on_error=0:detect_odr_violation=1"
 
@@ -57,10 +70,35 @@ function execute {
   #kcachegrind perf.callgrind
   #perf stat \
   #perf record -g --call-graph 'fp' -- \
-  ./raytracer \
+  ${BIN_DIR}/AppInterface \
             ${THREAD} ${SHADER} ${SCENE} ${SPP} ${SPL} ${WIDTH} ${HEIGHT} ${ACC} ${REP} \
             ${OBJ} ${MTL} ${PRINT} ${ASYNC} ${SHOWIMAGE}
   #perf report -g '' --show-nr-samples --hierarchy
+}
+
+function clangtidy {
+  clang-tidy-5.0 \
+	-analyze-temporary-dtors \
+	-checks='*,-*llvm-header-guard*' \
+	-header-filter='.*' \
+	-p \
+	${MOBILERT_SRCS}/*/*.*pp \
+  ${MOBILERT_SRCS}/*/*/*.*pp \
+  ${COMPONENTS_SRCS}/*/*/*.*pp \
+	${GTK_INTERFACE_SRCS}/*.*pp \
+  ${SCENES_SRCS}/*.*pp \
+	-- -std=c++14 -ferror-limit=1 \
+  -I ${MOBILERT_SRCS} \
+  -I ${COMPONENTS_SRCS} \
+  -I ${GTK_INTERFACE_SRCS} \
+  -I ${SCENES_SRCS} \
+	-isystem ${COMPONENTS_THIRDPARTY} \
+	-isystem /usr/include/c++/v1 \
+	-isystem /usr/lib/gcc/x86_64-linux-gnu/5/include \
+  -isystem /usr/include/glib-2.0/gobject \
+  -isystem /usr/include/gtk-2.0/gtk \
+	${GTK_HEADERS} \
+	2>&1 | tee ${SCRIPTS_DIR}/tidy.out
 }
 
 function profile {
@@ -84,10 +122,10 @@ function profile {
 
             PLOT_FILE="SC${SCENE}${SEP}SH${SHADER}${SEP}A${ACC}${SEP}R${WIDTH}x${HEIGHT}"
 
-            ./raytracer \
+            ${BIN_DIR}/AppInterface \
             ${THREAD} ${SHADER} ${SCENE} ${SPP} ${SPL} ${WIDTH} ${HEIGHT} ${ACC} ${REP} \
             ${OBJ} ${MTL} ${PRINT} ${ASYNC} ${SHOWIMAGE} \
-            | awk -v threads="${THREAD}" -f ${PLOT_SCRIPTS}/parser_out.awk 2>&1 \
+            | awk -v threads="${THREAD}" -f ${PLOT_SCRIPTS_DIR}/parser_out.awk 2>&1 \
             | tee -a ${PLOT_GRAPHS}/${PLOT_FILE}.dat
 
           done
@@ -103,15 +141,17 @@ PARAM2="drawt"
 PARAM3="draws"
 PARAM4="test"
 PARAM5="exec"
+PARAM6="tidy"
 
 for P in ${@}
 do
   case ${P} in
     ${PARAM1}) profile; sleep 2s ;;
-    ${PARAM2}) . ${PLOT_SCRIPTS}/plot.sh 0;;
-    ${PARAM3}) . ${PLOT_SCRIPTS}/plot.sh 1;;
-    ${PARAM4}) awk -f "${PLOT_SCRIPTS}/parser_median.awk" "${PLOT_SCRIPTS}/test.dat"  ;;
+    ${PARAM2}) . ${PLOT_SCRIPTS_DIR}/plot.sh 0;;
+    ${PARAM3}) . ${PLOT_SCRIPTS_DIR}/plot.sh 1;;
+    ${PARAM4}) awk -f "${PLOT_SCRIPTS_DIR}/parser_median.awk" "${PLOT_SCRIPTS_DIR}/test.dat"  ;;
     ${PARAM5}) execute ;;
+    ${PARAM6}) clangtidy ;;
     *) echo ""
        echo "Wrong Parameter: ${P}"
        echo "The valid parameters are:"
