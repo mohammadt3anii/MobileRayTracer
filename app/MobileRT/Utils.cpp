a1c3b5bf2189f604adfdc95ee4afc41ee430c90c
PTPuscas <ni_net@hotmail.com>
2018-02-25 18:23:40 +0000

    Fixed a bug in Utils.

--- app/MobileRT/Utils.cpp
+++ app/MobileRT/Utils.cpp
@@ -4,42 +4,45 @@
 
 #include "Utils.hpp"
 
-int ::MobileRT::roundDownToMultipleOf(const ::std::int32_t value, const ::std::int32_t multiple) noexcept {
-    const ::std::int32_t rest{value % multiple};
-    return rest > 1 ? value - rest : value;
-}
+namespace MobileRT {
 
-//https://en.wikipedia.org/wiki/Halton_sequence
-float ::MobileRT::haltonSequence(::std::uint32_t index, const ::std::uint32_t base) noexcept {
-    float f{1.0f};
-    float result{0.0f};
-    while (index > 0) {
-        f /= base;
-        result += f * (index % base);
-        index = static_cast<::std::uint32_t> (::std::floor(index / base));
+    ::std::int32_t roundDownToMultipleOf(const ::std::int32_t value, const ::std::int32_t multiple) noexcept {
+        const ::std::int32_t rest{value % multiple};
+        return rest > 1 ? value - rest : value;
     }
-    return result;
-}
 
-//newAvg = ((size - 1) * oldAvg + newNum) / size;
-unsigned ::MobileRT::incrementalAvg(const ::glm::vec3 sample, const ::std::uint32_t avg, const ::std::uint32_t numSample) noexcept {
-    //toneMap();
+    //https://en.wikipedia.org/wiki/Halton_sequence
+    float haltonSequence(::std::uint32_t index, const ::std::uint32_t base) noexcept {
+        float f{1.0f};
+        float result{0.0f};
+        while (index > 0) {
+            f /= base;
+            result += f * (index % base);
+            index = static_cast<::std::uint32_t> (::std::floor(index / base));
+        }
+        return result;
+    }
 
-    const ::std::uint32_t redB4{avg & 0xFF};
-    const ::std::uint32_t greenB4{(avg >> 8) & 0xFF};
-    const ::std::uint32_t blueB4{(avg >> 16) & 0xFF};
+    //newAvg = ((size - 1) * oldAvg + newNum) / size;
+    ::std::uint32_t incrementalAvg(const ::glm::vec3 sample, const ::std::uint32_t avg, const ::std::uint32_t numSample) noexcept {
+        //toneMap();
 
-    const ::std::uint32_t redSamp{static_cast<::std::uint32_t> (sample.r * 255u)};
-    const ::std::uint32_t greenSamp{static_cast<::std::uint32_t> (sample.g * 255u)};
-    const ::std::uint32_t blueSamp{static_cast<::std::uint32_t> (sample.b * 255u)};
+        const ::std::uint32_t redB4{avg & 0xFF};
+        const ::std::uint32_t greenB4{(avg >> 8) & 0xFF};
+        const ::std::uint32_t blueB4{(avg >> 16) & 0xFF};
 
-    const ::std::uint32_t redAft{((numSample - 1) * redB4 + redSamp) / numSample};
-    const ::std::uint32_t greenAft{((numSample - 1) * greenB4 + greenSamp) / numSample};
-    const ::std::uint32_t blueAft{((numSample - 1) * blueB4 + blueSamp) / numSample};
+        const ::std::uint32_t redSamp{static_cast<::std::uint32_t> (sample.r * 255u)};
+        const ::std::uint32_t greenSamp{static_cast<::std::uint32_t> (sample.g * 255u)};
+        const ::std::uint32_t blueSamp{static_cast<::std::uint32_t> (sample.b * 255u)};
 
-    const ::std::uint32_t retR {::std::min(redAft, 255u)};
-    const ::std::uint32_t retG {::std::min(greenAft, 255u)};
-    const ::std::uint32_t retB {::std::min(blueAft, 255u)};
+        const ::std::uint32_t redAft{((numSample - 1) * redB4 + redSamp) / numSample};
+        const ::std::uint32_t greenAft{((numSample - 1) * greenB4 + greenSamp) / numSample};
+        const ::std::uint32_t blueAft{((numSample - 1) * blueB4 + blueSamp) / numSample};
 
-    return (0xFF000000u | (retB << 16u) | (retG << 8u) | retR);
+        const ::std::uint32_t retR {::std::min(redAft, 255u)};
+        const ::std::uint32_t retG {::std::min(greenAft, 255u)};
+        const ::std::uint32_t retB {::std::min(blueAft, 255u)};
+
+        return (0xFF000000u | (retB << 16u) | (retG << 8u) | retR);
+    }
 }
