d0edb3e3ffd98d07c0a1670daba92559b249ab4c
Tiago <tiago92_net@hotmail.com>
2018-05-10 15:14:53 +0100

    Added the dissertation.

---
+++ Dissertation/clean.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+rm -rf *.acn
+rm -rf *.acr
+rm -rf *.alg
+rm -rf *.aux
+rm -rf *.glg
+rm -rf *.glo
+rm -rf *.gls
+rm -rf *.ist
+rm -rf *.lof
+rm -rf *.log
+rm -rf *.lol
+rm -rf *.lot
+rm -rf *.toc
+rm -rf *.synctex.gz
+rm -rf *.bbl
+rm -rf *.blg
+rm -rf *.upa
+rm -rf *.upb
+rm -rf *.pdf
